902dabdb884637ea893050248adff4a55a2eaf85
ArcaneCipher <desjardins.jarp@gmail.com>
2024-10-07 00:33:27 -0400

    Created takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,41 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const takeUntil = function (array, callback) {
+  // Loop through and push array elements until element value from callback function is truthy.
+  result = [];
+
+  for (let item of array) {
+    // If the callback returns true, stop and return the result
+    if (callback(item)) {
+      break;
+    }
+    // Otherwise, push the item to the result array
+    result.push(item);
+  }
+  return result; // Return the collected results
+};
+
+// Test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); // Should pass
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]); // Should pass
