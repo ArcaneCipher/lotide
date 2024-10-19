1a1595b1b06ae0d233e395a9fea063cf66d75b05
ArcaneCipher <desjardins.jarp@gmail.com>
2024-10-18 23:53:23 -0400

    forgot map function for index.js. sorted functions alphabetically.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
@@ -11,15 +8,16 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head   = require('./head');
 const letterPositions = require('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const tail   = require('./tail');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
-const flatten = require('./flatten');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
   assertArraysEqual: assertArraysEqual,
   assertEqual: assertEqual,
   assertObjectsEqual: assertObjectsEqual,
@@ -29,8 +27,12 @@ module.exports = {
   eqObjects: eqObjects,
   findKey: findKey,
   findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head:   head,
   letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail:   tail,
   takeUntil: takeUntil,
-  without: without,
-  flatten: flatten
+  without: without
 };
